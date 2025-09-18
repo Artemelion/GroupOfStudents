850d6405904f969e6ce7f230cfeeb1c6e580cb4c
Artemeleon Morarevich <artyommorar@gmail.com>
2025-09-18 04:50:26 +0300

    show methods in main

--- GroupOfStudents/Main.cpp
+++ GroupOfStudents/Main.cpp
@@ -33,7 +33,7 @@ int main()
     st2->setName("Andrey");
     group.AddStudent(st2);
 
-    group.SetMark(1, 0, 8); // we have subjectCount == marksCount
+    group.SetMark(1, 0, 8); // we have subjectCount == marksCount now
     group.SetMark(1, 1, 7);
     group.SetMark(1, 2, 5);
 
