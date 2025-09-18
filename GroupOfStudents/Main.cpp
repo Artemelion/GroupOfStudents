cc4341fc19613bbb8623ebdb9c8a41d42eeb1381
Artemeleon Morarevich <artyommorar@gmail.com>
2025-09-18 04:50:02 +0300

    show methods in main

--- GroupOfStudents/Main.cpp
+++ GroupOfStudents/Main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 #include "Subject.h"
 #include "Student.h"
+#include "Group.h"
 using namespace std;
-
-/*
-Написать программу «Группа студентов».
-Программа получает следующие данные:
-список студентов группы,
-список предметов для группы,
-таблицу оценок студентов группы по всем предметам.
-
-Программа выводит следующие данные:
-таблицу оценок студентов по предметам,
-средние оценки студентов,
-средние оценки по предметам,
-средний балл группы,
-максимальные и минимальные оценки по предметам 
-с указанием студентов.
-
-Разработать и использовать в программе классы:
-«Группа», «Студент», «Предмет».
-
-Атрибуты группы: название, список студентов, 
-список предметов.
-Атрибуты студента: имя, 
-список оценок студента по конкретному предмету.
-Атрибуты предмета: название.
-
-
-*/
 
 
 int main()
 {
-	//Subject subject1("Math");
-	//subject1.Print();
-	//int marks1[] = { 5,4,3,5,4 };
-	//Student student1("Ivan", marks1, 5);
+    Group group("KH-241");
+
+    Subject* s1 = new Subject("Programming");
+    Subject* s2 = new Subject("Operating systems");
+    Subject* s3 = new Subject("Mathematics");
+
+    group.AddSubject(s1);
+    group.AddSubject(s2);
+    group.AddSubject(s3);
+
+    Student* st1 = new Student("Artyom", group.getSubjectsCount());
+    group.AddStudent(st1);
+	group.SetMark(0, 0, 12); // we have subjectCount == marksCount
+    group.SetMark(0, 1, 11);
+    group.SetMark(0, 2, 10);
+
+
+
+
+    Student* st2 = new Student(*st1); 
+
+
+
+    st2->setName("Andrey");
+    group.AddStudent(st2);
+
+    group.SetMark(1, 0, 8); // we have subjectCount == marksCount
+    group.SetMark(1, 1, 7);
+    group.SetMark(1, 2, 5);
+
+    group.PrintMarksTable();
+    group.PrintAverages();
+    group.PrintMinMaxBySubject();
 
 	return 0;
 }
